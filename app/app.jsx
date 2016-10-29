461720e296ebca59722e821947ba490a15a69da8
Andrey Makarov <vivalaakam@gmail.com>
2016-10-29 09:20:38 +0300

    initial

--- app/app.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Router from 'react-router';
-import router from './router';
-
-import '../less/app.less';
-
-router.run(function(Handler) {
-    React.render(<Handler/>, document.getElementById('main'));
-});
