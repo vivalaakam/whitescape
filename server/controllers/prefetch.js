c027043a30483bc12f1a7c76d4edc8ba8226d79f
Andrey Makarov <vivalaakam@gmail.com>
2016-11-04 14:00:03 +0300

    webpack-isomorphic-tools

--- server/controllers/prefetch.js
+++ server/controllers/prefetch.js
@@ -3,21 +3,25 @@ import Message from '../models/message';
 
 export default {
   messages: async function fetchMessages(ctx, next) {
-    const model = new Message();
-    const auth = ctx.state.user;
-    const perPage = 10;
-    const list = await model.list({ user_id: auth.id }, [['created_at', 'DESC']], perPage);
-    const count = await model.count({ user_id: auth.id });
-    ctx.prefetch = {
-      messages: {
-        list,
-        params: {
-          loadMore: count > perPage,
-          perPage,
-          lastTime: R.last(list).created_at
+    try {
+      const model = new Message();
+      const auth = ctx.state.user;
+      const perPage = 10;
+      const list = await model.list({ user_id: auth.id }, [['created_at', 'DESC']], perPage);
+      const count = await model.count({ user_id: auth.id });
+      ctx.prefetch = {
+        messages: {
+          list,
+          params: {
+            loadMore: count > perPage,
+            perPage,
+            lastTime: R.last(list).created_at
+          }
         }
-      }
-    };
-    next();
+      };
+      next();
+    } catch (e) {
+      next();
+    }
   }
 };
